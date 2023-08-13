61cc36f66a3796d93f5ebdeecce5d439486976c3
Ernesto_11 <galindohernandezernesto@gmail.com>
2023-08-13 16:28:12 -0700

    Consultar, modificar y eliminar de los Formularios deporte y datosclimaticos terminado

--- js/datosclimaticos.js
+++ js/datosclimaticos.js
@@ -1,3 +1,4 @@
+// Limpiar
 document.getElementById("btn-limpiar").addEventListener("click", function(event) {
     document.getElementById("idRecoM").value = "";
     document.getElementById("temperaturaDC").value = "";
@@ -7,7 +8,144 @@ document.getElementById("btn-limpiar").addEventListener("click", function(event)
     document.getElementById("idRecoPDC").value = "";
 });
 
-document.getElementById("usuario").addEventListener("submit", function(event) {
+// Consultar
+document.getElementById("btn-consultar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "DC_Id"
+    const DC_Id = document.getElementById("DC_Id").value;
+
+    // Realiza una solicitud GET a la API para obtener los datos correspondientes al ID
+    fetch(`http://localhost:3000/api/datosclimaticos/${DC_Id}`)
+        .then(response => {
+            if (!response.ok) {
+                document.getElementById("idRecoM").value = "";
+                document.getElementById("temperaturaDC").value = "";
+                document.getElementById("humedadDC").value = "";
+                document.getElementById("nivelLluviaDC").value = "";
+                document.getElementById("indiceUV").value = "";
+                document.getElementById("idRecoPDC").value = "";
+                throw new Error("El ID ingresado no existe en la base de datos");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Llena los campos con los datos obtenidos
+            document.getElementById("idRecoM").value = data.DC_IdRecoM;
+            document.getElementById("temperaturaDC").value = data.DC_Temperatura;
+            document.getElementById("humedadDC").value = data.DC_Humedad;
+            document.getElementById("nivelLluviaDC").value = data.DC_NivelLluvia;
+            document.getElementById("indiceUV").value = data.DC_IndiceUV;
+            document.getElementById("idRecoPDC").value = data.DC_IdRecoP;
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+            alert(error.message); // Muestra el mensaje de error
+        });
+
+    cargarTabla();
+});
+
+// Modificar
+document.getElementById("btn-modificar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "DC_Id"
+    const DC_Id = document.getElementById("DC_Id").value;
+    
+    // Validación: Asegurarse de que se ingrese un ID válido
+    if (!DC_Id) {
+        alert("Por favor, ingrese un ID para consultar");
+        return;
+    }
+
+    // Realiza una solicitud GET a la API para verificar si el ID existe
+    fetch(`http://localhost:3000/api/datosclimaticos/${DC_Id}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("El ID ingresado no existe en la base de datos");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Verifica que todos los campos tengan información
+            const idRecoM = document.getElementById("idRecoM").value;
+            const temperatura = document.getElementById("temperaturaDC").value;
+            const humedad = document.getElementById("humedadDC").value;
+            const nivelLluvia = document.getElementById("nivelLluviaDC").value;
+            const indiceUV = document.getElementById("indiceUV").value;
+            const idRecoPDC = document.getElementById("idRecoPDC").value;
+
+            if (!idRecoM || !temperatura || !humedad || !nivelLluvia || !indiceUV || !idRecoPDC) {
+                alert("Por favor, complete todos los campos antes de modificar");
+                return;
+            }
+
+            // Crea un objeto con los datos modificados
+            const dataToModify = {
+                idRecoM: idRecoM,
+                temperatura: temperatura,
+                humedad: humedad,
+                nivelLluvia: nivelLluvia,
+                indiceUV: indiceUV,
+                idRecoPDC: idRecoPDC
+            };
+
+            // Realiza una solicitud PUT a la API para modificar el registro
+            fetch(`http://localhost:3000/api/datosclimaticos/${DC_Id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(dataToModify)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Respuesta de la API:", data);
+                alert("Registro modificado correctamente");
+
+                // Actualizar la tabla con los nuevos datos
+                cargarTabla();
+            })
+            .catch(error => {
+                console.error("Error al modificar el registro:", error);
+                alert("Ocurrió un error al modificar el registro");
+            });
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+            alert(error.message);
+        });
+});
+
+// Borrar
+document.getElementById("btn-borrar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "DC_Id"
+    const DC_Id = document.getElementById("DC_Id").value;
+
+    // Realiza una solicitud DELETE a la API para borrar el registro correspondiente al ID
+    fetch(`http://localhost:3000/api/datosclimaticos/${DC_Id}`, {
+        method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro eliminado correctamente");
+        
+        // Actualizar la tabla con los nuevos datos después del borrado
+        cargarTabla();
+    })
+    .catch(error => {
+        console.error("Error al borrar el registro:", error);
+        alert("Ocurrió un error al borrar el registro");
+    });
+});
+
+
+
+document.getElementById("datosclimaticos").addEventListener("submit", function(event) {
     event.preventDefault();
 
     // Captura los valores de los campos
--- js/deporte.js
+++ js/deporte.js
@@ -1,5 +1,114 @@
+// Limpiar
 document.getElementById("btn-limpiar").addEventListener("click", function(event) {
     document.getElementById("nombreDeporte").value = "";
+});
+
+// Consultar
+document.getElementById("btn-consultar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "Depor_Id"
+    const Depor_Id = document.getElementById("Depor_Id").value;
+
+    // Realiza una solicitud GET a la API para obtener los datos correspondientes al ID
+    fetch(`http://localhost:3000/api/deporte/${Depor_Id}`)
+        .then(response => {
+            if (!response.ok) {
+                document.getElementById("nombreDeporte").value = "";
+                throw new Error("El ID ingresado no existe en la base de datos");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Llena los campos con los datos obtenidos
+            document.getElementById("nombreDeporte").value = data.Depor_Nombre;
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+            alert(error.message); // Muestra el mensaje de error
+        });
+
+        cargarTablaDeporte();
+});
+
+// Modificar
+document.getElementById("btn-modificar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "Depor_Id"
+    const Depor_Id = document.getElementById("Depor_Id").value;
+
+    // Captura el valor del campo "nombreDeporte"
+    const nombreDeporte = document.getElementById("nombreDeporte").value;
+
+    // Validación: Asegurarse de que se ingrese un ID válido
+    if (!Depor_Id) {
+        alert("Por favor, ingrese un ID para consultar");
+        return;
+    }
+
+    // Validación: Asegurarse de que el campo nombreDeporte tenga un valor
+    if (!nombreDeporte) {
+        alert("Por favor, complete todos los campos antes de modificar");
+        return;
+    }
+
+    // Crea un objeto con los datos modificados
+    const dataToModify = {
+        Depor_Nombre: nombreDeporte
+    };
+
+    // Realiza una solicitud PUT a la API para modificar el registro
+    fetch(`http://localhost:3000/api/deporte/${Depor_Id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToModify)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al modificar el registro");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro modificado correctamente");
+
+        // Actualizar la tabla con los nuevos datos
+        cargarTablaDeporte();
+    })
+    .catch(error => {
+        console.error("Error al modificar el registro:", error);
+        alert("Ocurrió un error al modificar el registro");
+    });
+});
+
+
+// Borrar
+document.getElementById("btn-borrar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "DC_Id"
+    const Depor_Id = document.getElementById("Depor_Id").value;
+
+    // Realiza una solicitud DELETE a la API para borrar el registro correspondiente al ID
+    fetch(`http://localhost:3000/api/deporte/${Depor_Id}`, {
+        method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro eliminado correctamente");
+        
+        // Actualizar la tabla con los nuevos datos después del borrado
+        cargarTablaDeporte();
+    })
+    .catch(error => {
+        console.error("Error al borrar el registro:", error);
+        alert("Ocurrió un error al borrar el registro");
+    });
 });
 
 // Tabla Deporte
