33be66efc57ea43a12153ca2d0e5379c58bb0131
Juan Carlos Rivas <juan_carlosr223@hotmail.com>
2023-08-13 19:38:31 -0600

    Tabla Recomendación Protocolo Terminada

--- js/recomendacionprotocolo.js
+++ js/recomendacionprotocolo.js
@@ -1,8 +1,116 @@
+// Limpiar
 document.getElementById("btn-limpiar").addEventListener("click", function(event) {
     document.getElementById("nombreRecomendacionProtocolo").value = "";;
 });
 
-// Tabla EspacioDeportivo
+// Consultar
+document.getElementById("btn-consultar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "Depor_Id"
+    const RecoP_Id = document.getElementById("RecoP_Id").value;
+
+    // Realiza una solicitud GET a la API para obtener los datos correspondientes al ID
+    fetch(`http://localhost:3000/api/recomendacionprotocolo/${RecoP_Id}`)
+        .then(response => {
+            if (!response.ok) {
+                document.getElementById("nombreRecomendacionProtocolo").value = "";
+                throw new Error("El ID ingresado no existe en la base de datos");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Llena los campos con los datos obtenidos
+            document.getElementById("nombreRecomendacionProtocolo").value = data.RecoP_Protocolo;
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+            alert(error.message); // Muestra el mensaje de error
+        });
+
+        cargarTablaRecomendacionProtocolo();
+});
+
+// Modificar
+document.getElementById("btn-modificar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "Depor_Id"
+    const RecoP_Id = document.getElementById("RecoP_Id").value;
+
+    // Captura el valor del campo "nombreDeporte"
+    const nombreRecomendacionProtocolo = document.getElementById("nombreRecomendacionProtocolo").value;
+
+    // Validación: Asegurarse de que se ingrese un ID válido
+    if (!RecoP_Id) {
+        alert("Por favor, ingrese un ID para consultar");
+        return;
+    }
+
+    // Validación: Asegurarse de que el campo nombreDeporte tenga un valor
+    if (!nombreRecomendacionProtocolo) {
+        alert("Por favor, complete todos los campos antes de modificar");
+        return;
+    }
+
+    // Crea un objeto con los datos modificados
+    const dataToModify = {
+        RecoP_Protocolo: nombreRecomendacionProtocolo
+    };
+
+    // Realiza una solicitud PUT a la API para modificar el registro
+    fetch(`http://localhost:3000/api/recomendacionprotocolo/${RecoP_Id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToModify)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al modificar el registro");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro modificado correctamente");
+
+        // Actualizar la tabla con los nuevos datos
+        cargarTablaRecomendacionProtocolo();
+    })
+    .catch(error => {
+        console.error("Error al modificar el registro:", error);
+        alert("Ocurrió un error al modificar el registro");
+    });
+});
+
+// Borrar
+document.getElementById("btn-borrar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "DC_Id"
+    const RecoP_Id = document.getElementById("RecoP_Id").value;
+
+    // Realiza una solicitud DELETE a la API para borrar el registro correspondiente al ID
+    fetch(`http://localhost:3000/api/recomendacionprotocolo/${RecoP_Id}`, {
+        method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro eliminado correctamente");
+        
+        // Actualizar la tabla con los nuevos datos después del borrado
+        cargarTablaRecomendacionProtocolo();
+    })
+    .catch(error => {
+        console.error("Error al borrar el registro:", error);
+        alert("Ocurrió un error al borrar el registro");
+    });
+});
+
+// Tabla RecomendacionProtocolo
 const contenidoElementRecomendacionProtocolo = document.getElementById("contenidoRecomendacionProtocolo");
 
 function cargarTablaRecomendacionProtocolo() {
