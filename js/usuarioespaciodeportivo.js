a8d1303404c1a439dd6674b6ec6f99ff8cd58f91
Ernesto_11 <galindohernandezernesto@gmail.com>
2023-08-13 18:56:44 -0700

    Consultar, modificar y eliminar de los Formularios usuarioespaciodeportivo terminado

--- js/usuarioespaciodeportivo.js
+++ js/usuarioespaciodeportivo.js
@@ -1,6 +1,118 @@
+// Limpiar
 document.getElementById("btn-limpiar").addEventListener("click", function(event) {
     document.getElementById("UED_Uio_Id").value = "";
     document.getElementById("UED_ED_Id").value = "";
+});
+
+// Consultar
+document.getElementById("btn-consultar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "Depor_Id"
+    const UED_Id = document.getElementById("UED_Id").value;
+
+    // Realiza una solicitud GET a la API para obtener los datos correspondientes al ID
+    fetch(`http://localhost:3000/api/usuarioespaciodeportivo/${UED_Id}`)
+        .then(response => {
+            if (!response.ok) {
+                document.getElementById("UED_Uio_Id").value = "";
+                document.getElementById("UED_ED_Id").value = "";
+                throw new Error("El ID ingresado no existe en la base de datos");
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Llena los campos con los datos obtenidos
+            document.getElementById("UED_Uio_Id").value = data.UED_Uio_Id;
+            document.getElementById("UED_ED_Id").value = data.UED_ED_Id;
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos:", error);
+            alert(error.message); // Muestra el mensaje de error
+        });
+
+        cargarTabla();
+});
+
+// Modificar
+document.getElementById("btn-modificar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // Captura el valor del campo "Depor_Id"
+    const UED_Id = document.getElementById("UED_Id").value;
+
+    // Captura el valor del campo "nombreDeporte"
+    const UED_Uio_Id = document.getElementById("UED_Uio_Id").value;
+    const UED_ED_Id = document.getElementById("UED_ED_Id").value;
+
+    // Validación: Asegurarse de que se ingrese un ID válido
+    if (!UED_Id) {
+        alert("Por favor, ingrese un ID para consultar");
+        return;
+    }
+
+    if (!UED_Uio_Id || !UED_ED_Id) {
+        alert("Por favor, complete todos los campos antes de modificar");
+        return;
+    }
+
+    // Crea un objeto con los datos modificados
+    const dataToModify = {
+        UED_Uio_Id: UED_Uio_Id,
+        UED_ED_Id: UED_ED_Id
+    };
+
+    // Realiza una solicitud PUT a la API para modificar el registro
+    fetch(`http://localhost:3000/api/usuarioespaciodeportivo/${UED_Id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataToModify)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Error al modificar el registro");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro modificado correctamente");
+
+        // Actualizar la tabla con los nuevos datos
+        cargarTabla();
+    })
+    .catch(error => {
+        console.error("Error al modificar el registro:", error);
+        alert("Ocurrió un error al modificar el registro");
+    });
+});
+
+
+// Borrar
+document.getElementById("btn-borrar").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    
+    const UED_Id = document.getElementById("UED_Id").value;
+
+    // Realiza una solicitud DELETE a la API para borrar el registro correspondiente al ID
+    fetch(`http://localhost:3000/api/usuarioespaciodeportivo/${UED_Id}`, {
+        method: "DELETE"
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Respuesta de la API:", data);
+        alert("Registro eliminado correctamente");
+        
+        // Actualizar la tabla con los nuevos datos después del borrado
+        cargarTabla();
+    })
+    .catch(error => {
+        console.error("Error al borrar el registro:", error);
+        alert("Ocurrió un error al borrar el registro");
+    });
 });
 
 document.getElementById("usuarioespaciodeportivo").addEventListener("submit", function(event) {
